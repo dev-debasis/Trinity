9771be302329ee14268c7dffb2bde52b98243520
Aman Prasad <110687183+AmanPrasad88@users.noreply.github.com>
2024-09-26 20:58:32 +0530

    feat: Integrate completed frontend from temporary repository

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,39 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'black-rgb': 'rgb(32, 38, 55)',
+      },
+      keyframes: {
+        'border-spin': {
+          '100%': {
+            transform: 'rotate(-360deg)',
+          },
+        },
+      },
+      animation: {
+        'border-spin': 'border-spin 7s linear infinite',
+      },
+    },
+  },
+  plugins: [
+    function ({addUtilities}) {
+      const newUtilities = {
+        ".no-scrollbar::-webkit-scrollbar":{
+          display: "none",
+        },
+        ".no-scrollbar": {
+          "-ms-overflow-style": "none",
+          "scrollbar-width": "none",
+        },
+      };
+
+      addUtilities(newUtilities);
+    }
+  ],
+}
